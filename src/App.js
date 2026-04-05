059df22d00d7af7d77e8c56055d83eb98334687e
Sau694 <sauravbagul23@gmail.com>
2026-04-05 20:27:18 +0530

    Update API endpoint to production URL

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://fake-news-backend.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
